31a87867a15d11f9f594ec16ce15fed99ea3cd60
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-08-16 11:10:13 +0000

    Variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "hittesh@google.com"
+var accountPassword = "Jaipur"
+// accountCity = "Jaiput"
+// accountId = 2 //not allowed
+let accounttState;
+
+
+// console.log(accountId);
+// console.log(accountId);
+// console.log(accountPassword)
+
+accountEmail = "sumanmanna048@gmail.com"
+accountPassword = "2121525"
+
+console.table([accountId, accountEmail, accountPassword, accounttState])
+
+/*
+prefer not to  use var
+because of issue block scope and functional scope*/
