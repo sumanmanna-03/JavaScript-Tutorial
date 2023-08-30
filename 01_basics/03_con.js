81ae9c5145991c6189db2c8bab89254dff991130
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-08-30 09:30:43 +0000

    done

---
+++ 01_basics/03_con.js
@@ -0,0 +1,32 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc"=> give value NaN
+// true => 1 , false => 0
+
+// let isLoggedIn = 1
+
+// let booleanInLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanInLoggedIn)
+
+let isLoggedIn = "hitesh"
+
+let booleanInLoggedIn = Boolean(isLoggedIn)
+console.log(booleanInLoggedIn)
+
+//1 => true
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
