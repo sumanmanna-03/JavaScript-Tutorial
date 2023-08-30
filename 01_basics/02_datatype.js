044f70b7eb2a2550eac1b4cf532745fd774ae770
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-08-30 08:30:38 +0000

    datatype done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,11 @@
+//number => 2 to power 53
+//bigint
+//String =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof "Suman manna")
