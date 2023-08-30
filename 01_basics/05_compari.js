ade9921456798108de8e1c9594c262fc88ef3afa
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-08-30 11:03:38 +0000

    datatype sumery

---
+++ 01_basics/05_compari.js
@@ -0,0 +1,30 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2 == 1)
+// console.log(2!=1)
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+// {
+    
+// console.log(null>0) // false  //here null converse in NaN or 0
+// console.log(null == 0) //false
+// console.log(null >= 0) //true
+
+// console.log(undefined == 0) //false
+// console.log(undefined > 0) //false
+// console.log(undefined < 0) //false  
+
+// this is type conversion should be avoid   always should be clean code
+
+// }
+
+//
+
+
+// === (strict check)
+
+console.log("2" == 2) // true
+console.log("2" === 2) //false
---
+++ 01_basics/datatype.js
@@ -0,0 +1,47 @@
+// # primitive
+
+//7 types : String, Number, Boolean, null, undefiened, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+//Symbol declare
+
+// const id = symbol('123')
+// const anotherId = symbol('123')
+
+// console.log(id === anotherId)
+
+//bigInt
+
+const bigNumber = 65646546556564656n
+console.log(bigNumber)
+
+// # reference or Non-primitive
+
+
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+console.log(heros)
+
+let myObj = {
+    namme : "Suman",
+    age : 22
+}
+console.log(myObj)
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+console.log(myFunction)
+ 
+
+console.log(typeof bigNumber)  // bignit
+console.log(typeof myFunction) //function
+console.log(null) // object
