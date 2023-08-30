d49ad8578cac13e1b4001a3c91914790b707a07e
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-08-30 19:13:04 +0000

    done

---
+++ 01_basics/string.js
@@ -0,0 +1,13 @@
+const name = "hitesh"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gamename = new String('hitesh')
+
+console.log(gamename[0])
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase())
+console.group(gamename.charAt(2))
+console.log(gamename.indexOf('t'))
