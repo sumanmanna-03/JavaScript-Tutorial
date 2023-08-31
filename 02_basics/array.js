088a70ca2d2a8ecaffc0e86a1ea54d560bb27839
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-08-31 04:06:04 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,11 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+console.log(myArr[0]);
+const myHeroes = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3)
+console.log(myArr2[0]);
+
+//Array method
+
+myArr.push(7)
+console.log(myArr);
