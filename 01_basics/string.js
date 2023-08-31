f2526d7c3827a841d8f42688e2890d695cb9bb62
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-08-31 03:19:59 +0000

    done

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -3,11 +3,31 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
 
-const gamename = new String('hitesh')
+const gamename = new String('hitesh-hc')
 
 console.log(gamename[0])
 
-console.log(gamename.length);
-console.log(gamename.toUpperCase())
-console.group(gamename.charAt(2))
-console.log(gamename.indexOf('t'))
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase())
+// console.group(gamename.charAt(2))
+// console.log(gamename.indexOf('t'))
+
+const newGame = gamename.substring(1,3)
+console.log(newGame);
+
+const anotherString = gamename.slice(-8,4)
+console.log(anotherString);
+
+const newString = "    hitesh    "
+console.log(newString)
+console.log(newString.trim())
+
+
+const url = "https://hitesh.com/hitesh%20choudhury"
+
+// url.replace('%20', '-')
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('hitesh'))
+
+console.log(gamename.split('-'));
