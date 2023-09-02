df167623cb2b25ccac232efbaca633a70ce79775
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-09-02 13:09:54 +0000

    switch

--- controlflow/switch.js
+++ controlflow/switch.js
@@ -1,7 +1,28 @@
-switch(key){
-    case vlaue;
-    break
+// switch(key){
+//     case vlaue;
+//     break
 
-    default ;
-    break;
+//     default ;
+//     break;
+// }
+
+const month = "Jan"
+
+switch(month){
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+        case "Mar":
+            console.log("March");
+            break;
+            case "April":
+                console.log("April")
+                break;
+            default:
+                console.log("default case is match")
+                break;
+
 }
