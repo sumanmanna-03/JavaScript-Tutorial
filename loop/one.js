b92117aa06f2f2edd891878e81a59321d29a52e6
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-09-03 14:32:25 +0000

    done

--- loop/one.js
+++ loop/one.js
@@ -1,9 +1,42 @@
-// for
+//for
 
-for (let index = 0; index < 10; index++) {
-    const element = index;
-    console.log(element);   
-}
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(element == 5){
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
 
 // console.log(element);
-for
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Order loop value: ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         // console.log(`Inner loop value ${j} and inner loop ${i}`);
+//         console.log(i + '*' + j + ' = ' + i*j );
+        
+//     }
+    
+// }
+
+let myArray = ["flash", "ironman", "batman", "superman"]
+// console.log(myArray.length);
+for (let index = 1; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+//break and continue
+
+for (let index = 1; index <= 20; index++) {
+    // const element = array[index];
+    if(index == 5){
+        console.log(`Detected 5`);
+        continue;
+    }
+    console.log(`Value of i is ${index}`);
+
+    
+}
