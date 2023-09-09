fcc35d0afecd5bd79b534413735b2414c3ee5b4a
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-09-09 04:59:56 +0000

    obj and json

---
+++ obj/three.js
@@ -0,0 +1,35 @@
+// Destructuring of object
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseinstructor: "hitesh"
+}
+
+// course.courseinstructor
+
+const {courseinstructor: instructor} = course
+ 
+// console.log(courseinstructor);
+console.log(instructor) 
+
+/*const navbar = ({company}) => {
+    
+}
+
+navbar(company = "hitesh")*/
+
+
+// API (Application Programming Interface)
+
+// {
+//     name: "hitesh",
+//     coursename: "js in hindi",
+//     price: "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
