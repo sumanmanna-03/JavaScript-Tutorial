9211b65cd1fcb9d1080c9654cbac8defd5289d33
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-09-09 08:51:42 +0000

    done

--- function/one.js
+++ function/one.js
@@ -19,12 +19,38 @@ function sayName(){
 // function addTwoNumber(number1, number2){
 //       console.log(number1 + number2);
 // }
+
 function addTwoNumber(number1, number2){
-      console.log(number1 + number2);
+    //   let result = number1 + number2
+    //   return result
+    
+      //   console.log("Hitesh");// return ka bad koi bhi code kaam nahi karta
+
+      return number1 + number2
 }
 
 // addTwoNumber(3, "4")
 // addTwoNumber(3, "a")
 const result = addTwoNumber(3, 5)
 
-console.log("Ressult: ", result);
+// console.log("Ressult: ", result);
+
+
+// function loginuserMessage(username){
+//     if(username === undefined){
+//         console.log("Please enter a user name");
+//         return
+//     }
+//      return `${username} just logged in`
+// }
+function loginuserMessage(username = "sam"){
+    if(!username){
+        console.log("Please enter a user name");
+        return
+    }
+     return `${username} just logged in`
+}
+
+
+console.log(loginuserMessage("Hitesh"))
+// console.log(loginuserMessage())
