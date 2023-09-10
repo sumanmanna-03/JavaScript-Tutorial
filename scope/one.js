fcea1393bd51df9b0dd19c91a2df4d8dec1066c2
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-09-10 14:24:46 +0000

    done

--- scope/one.js
+++ scope/one.js
@@ -16,6 +16,66 @@ if(true){
 // console.log(b);
 // console.log(c);
 
+//NESTED SCOPE     
+
 function one(){
-    
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
 }
+
+one()
+
+// Here under ka variable baar ka score ko access kar sakta hai but bahar ka variable scope under ka variable ko access nahi kar sakta
+
+// Child function can access parent variable - nested scope(closer)
+
+
+if (true){
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website = " youtube"
+        // console.log(username  + website);
+    }
+    // console.log(website) //it will throw error
+}
+
+// console.log(username) // it will throw error
+
+
+// +++++++++++++++++++++ INTERESTING +++++++++++++++++++++
+
+
+// foor this case addone can access before initialization
+
+addone(5)
+
+function addone(num){
+    return num + 1
+}
+
+
+// for this case addtwo cannot access before initialiation
+
+addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+
+
+
+
+
+
+
+
+
+
