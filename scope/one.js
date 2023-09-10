639a44e8d9fd32e9faec09ffcee830a1d1f6f06b
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-09-10 12:28:39 +0000

    done

---
+++ scope/one.js
@@ -0,0 +1,21 @@
+// scope start with let . var, const
+let a = 30
+const b = 20
+var c = 30
+
+if(true){
+    let a = 20
+    const b = 20
+    var c = 30
+}
+
+// if ka under jo bhi ha wo ha block scope
+// if ka bahar jo bhi ha wo hai Global scope
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    
+}
