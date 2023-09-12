0c960087d935cca805157684abb62f5d52cd4dd5
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-09-12 16:22:38 +0000

    done

---
+++ dates.js
@@ -0,0 +1,17 @@
+//Date
+
+let myDate = new Date()
+console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toJSON())
+// console.log(myDate)
+
+console.log(typeof myDate)    // here Date is a object  
+
+let myCreatedDate = new Date(2023, 0, 23)
+console.log(myCreatedDate);
+
