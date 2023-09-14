a084699afdec8131d1504ca45576708db2230d06
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-09-14 11:56:06 +0000

    done

---
+++ loop/a.js
@@ -0,0 +1,25 @@
+//for of loop
+
+const arr = [1, 2, 3, 4]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "Hello World"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
+}
+
+//Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United State of America")
+map.set('Fr', "France")
+
+// console.log(map)
+
+for (const key of map) {
+    console.log(key)
+}
